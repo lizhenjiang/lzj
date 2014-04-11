2e2be8191f38c6810a407610e145c003ddbdc86e
LZJ <lzj@lzj-VirtualBox.(none)>
2014-04-11 14:29:15 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lzj::Application.config.secret_token = 'fd29bd8e153bb896f635e7849c6a43c1887d11ffeeb27fbadc711e35147fac0722c79c309f9f11a43d01d56d668b89f98cdefa4f2c95a9ebc8c77b5003b8fe77'
