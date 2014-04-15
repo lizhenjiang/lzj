38585117ee780989449fb2d5ec7a587441814723
LZJ <lzj@lzj-VirtualBox.(none)>
2014-04-15 15:06:58 +0800

    Initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,27 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Lzj::Application.config.secret_token = 'fd29bd8e153bb896f635e7849c6a43c1887d11ffeeb27fbadc711e35147fac0722c79c309f9f11a43d01d56d668b89f98cdefa4f2c95a9ebc8c77b5003b8fe77'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Lzj::Application.config.secret_key_base = secure_token
